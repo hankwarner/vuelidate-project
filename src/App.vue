9a39fda9372a1a2be3f9e0c9c638b422be08473b
robertromaine-AngelOak <58518291+robertromaine-AngelOak@users.noreply.github.com>
2021-01-12 14:19:55 -0500

    Created project template

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,78 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <b-container>
+        <b-card class="mt-3" header="Very Simple Form">
+            <b-form @submit="onSubmit">
+                <b-form-group label="Your Name:" :invalid-feedback="" :state="">
+                    <b-form-input
+                        v-model="form.name"
+                        placeholder="Enter name"
+                    ></b-form-input>
+                </b-form-group>
+
+                <b-form-group
+                    label="Email address:"
+                    :invalid-feedback=""
+                    :state=""
+                >
+                    <b-form-input
+                        v-model="form.email"
+                        placeholder="Enter email"
+                    ></b-form-input>
+                </b-form-group>
+
+                <b-form-group label="Phone:" :invalid-feedback="" :state="">
+                    <b-form-input
+                        v-model="form.phone"
+                        placeholder="Enter phone"
+                    ></b-form-input>
+                </b-form-group>
+
+                <b-form-group label="Food:" :invalid-feedback="" :state="">
+                    <b-form-select
+                        v-model="form.food"
+                        :options="foods"
+                    ></b-form-select>
+                </b-form-group>
+
+                <b-form-group :invalid-feedback="" :state="">
+                    <b-form-checkbox-group v-model="form.checked">
+                        <b-form-checkbox value="me"
+                            >Check me out</b-form-checkbox
+                        >
+                        <b-form-checkbox value="that"
+                            >Check that out</b-form-checkbox
+                        >
+                    </b-form-checkbox-group>
+                </b-form-group>
+
+                <b-button type="submit" variant="primary">Submit</b-button>
+            </b-form>
+        </b-card>
+    </b-container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    data() {
+        return {
+            form: {
+                name: "",
+                email: "",
+                phone: "",
+                food: null,
+                checked: [],
+            },
+            foods: [
+                { text: "Select One", value: null },
+                "Carrots",
+                "Beans",
+                "Tomatoes",
+                "Corn",
+            ],
+        };
+    },
+    validations: {},
+    methods: {},
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
